68c8c7714cd69050e0ebe2127a3f362e3cd78ddb
mofumofu5656 <134360590+fdtdengineer@users.noreply.github.com>
2026-07-20 15:33:41 +0900

    Expose imitation training API

--- engine/python/dqn/__init__.py
+++ engine/python/dqn/__init__.py
@@ -10,17 +10,35 @@ from .agent import (
     soft_update,
 )
 from .encoding import OBSERVATION_CHANNELS, encode_batch, encode_observation
+from .imitation import (
+    EpochMetrics,
+    ImitationConfig,
+    evaluate_imitation,
+    split_teacher_examples,
+    train_imitation,
+)
 from .model import DuelingQNetwork, ResidualBlock, count_trainable_parameters
 from .replay_buffer import ReplayBatch, ReplayBuffer, Transition
+from .symmetry import (
+    AllSymmetriesDataset,
+    SYMMETRY_COUNT,
+    transform_action,
+    transform_batch,
+    transform_observation,
+)
 from .teacher_data import TeacherDataset, TeacherExample, load_teacher_examples
 
 __all__ = [
+    "AllSymmetriesDataset",
     "DQNConfig",
     "DuelingQNetwork",
+    "EpochMetrics",
+    "ImitationConfig",
     "OBSERVATION_CHANNELS",
     "ReplayBatch",
     "ReplayBuffer",
     "ResidualBlock",
+    "SYMMETRY_COUNT",
     "TeacherDataset",
     "TeacherExample",
     "Transition",
@@ -28,10 +46,16 @@ __all__ = [
     "count_trainable_parameters",
     "encode_batch",
     "encode_observation",
+    "evaluate_imitation",
     "hard_update",
     "load_teacher_examples",
     "mask_illegal_actions",
     "optimize_double_dqn",
     "select_action",
     "soft_update",
+    "split_teacher_examples",
+    "train_imitation",
+    "transform_action",
+    "transform_batch",
+    "transform_observation",
 ]
